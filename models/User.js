628cb477449deef93a16cc2473c6170d10af865e
Cameron <cameron.wills89@outlook.com>
2020-10-26 10:24:53 -0400

    added models for user, comments and post

---
+++ models/User.js
@@ -0,0 +1,74 @@
+const { Model, DataTypes } = require("sequelize");
+const sequelize = require("../config/connection");
+//importing password hashing to protect users passwords from attacks
+const bcrypt = require("bcrypt");
+
+class User extends Model {
+  // set up method to run on instance data (per user) to check password
+  checkPassword(loginPw) {
+    return bcrypt.compareSync(loginPw, this.password);
+  }
+}
+
+// define table columns and configuration
+User.init(
+  {
+    // define an id column
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    // define a username column
+    username: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    // define an email column
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      // there cannot be any duplicate email values in this table
+      unique: true,
+      //allowNull is false, so we can validate emails before we create table
+      validate: {
+        isEmail: true,
+      },
+    },
+    // define a password column
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        len: [4],
+      },
+    },
+  },
+  {
+    //hash the password to protect it
+    //this hook needs to fire before a new user is created, so we use beforeCreate
+    hooks: {
+      // set up beforeCreate lifecycle "hook" functionality, with 10 saltRounds
+      async beforeCreate(newUserData) {
+        newUserData.password = await bcrypt.hash(newUserData.password, 10);
+        return newUserData;
+      },
+      // set up beforeUpdate lifecycle "hook" functionality if they want to change their password
+      async beforeUpdate(updatedUserData) {
+        updatedUserData.password = await bcrypt.hash(
+          updatedUserData.password,
+          10
+        );
+        return updatedUserData;
+      },
+    },
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: "user",
+  }
+);
+
+module.exports = User;
--- server.js
+++ server.js
@@ -2,44 +2,42 @@ const express = require("express");
 const routes = require("./controllers");
 const sequelize = require("./config/connection");
 const path = require("path");
-//allows us to use express-session and then link to sequelize store (for cookies)
-const session = require("express-session");
-const helpers = require("./utils/helpers");
-
-//setup Handlebars.js as the template engine
-const exphbs = require("express-handlebars");
-const hbs = exphbs.create({ helpers });
+// //import the handlebars helper functions
+// const helpers = require("./utils/helpers");
+// //setup Handlebars.js as the template engine
+// const exphbs = require("express-handlebars");
 
 const app = express();
-//setup environment variables
+// const hbs = exphbs.create({ helpers });
 const PORT = process.env.PORT || 3001;
-//session
-const SequelizeStore = require("connect-session-sequelize")(session.Store);
+//allows us to use express-session and then link to sequelize store (for cookies)
+// const session = require("express-session");
+// const SequelizeStore = require("connect-session-sequelize")(session.Store);
 
-//creating sess obj to save our session token into
-const sess = {
-  secret: "Super secret secret",
-  cookie: {},
-  resave: false,
-  saveUninitialized: true,
-  store: new SequelizeStore({
-    db: sequelize,
-  }),
-};
+//creating sess obj (short for session) to save our session token into
+// const sess = {
+//   secret: "Super secret secret",
+//   cookie: {},
+//   resave: false,
+//   saveUninitialized: true,
+//   store: new SequelizeStore({
+//     db: sequelize,
+//   }),
+// };
 
 
 //Middleware
-app.use(session(sess));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 //connect the css file
 app.use(express.static(path.join(__dirname, "public")));
+//sets up an Express.js session and connects the session to our Sequelize database
+// app.use(session(sess));
 
+// app.engine("handlebars", hbs.engine);
+// app.set("view engine", "handlebars");
 // turn on routes
-app.use(routes);
-
-app.engine("handlebars", hbs.engine);
-app.set("view engine", "handlebars");
+// app.use(routes);
 
 // turn on connection to db and server
 sequelize.sync({ force: false }).then(() => {
