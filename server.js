17908d0fea90d7e3f90263569a44b4cf98e315f2
Cameron <cameron.wills89@outlook.com>
2020-10-26 09:54:01 -0400

    finished file structure

--- server.js
+++ server.js
@@ -2,16 +2,18 @@ const express = require("express");
 const routes = require("./controllers");
 const sequelize = require("./config/connection");
 const path = require("path");
-//import the handlebars helper functions
+//allows us to use express-session and then link to sequelize store (for cookies)
+const session = require("express-session");
 const helpers = require("./utils/helpers");
 
 //setup Handlebars.js as the template engine
 const exphbs = require("express-handlebars");
 const hbs = exphbs.create({ helpers });
+
 const app = express();
+//setup environment variables
 const PORT = process.env.PORT || 3001;
-//allows us to use express-session and then link to sequelize store (for cookies)
-const session = require("express-session");
+//session
 const SequelizeStore = require("connect-session-sequelize")(session.Store);
 
 //creating sess obj to save our session token into
@@ -25,19 +27,19 @@ const sess = {
   }),
 };
 
-app.engine("handlebars", hbs.engine);
-app.set("view engine", "handlebars");
 
 //Middleware
+app.use(session(sess));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 //connect the css file
 app.use(express.static(path.join(__dirname, "public")));
 
-app.use(session(sess));
-
 // turn on routes
 app.use(routes);
+
+app.engine("handlebars", hbs.engine);
+app.set("view engine", "handlebars");
 
 // turn on connection to db and server
 sequelize.sync({ force: false }).then(() => {
